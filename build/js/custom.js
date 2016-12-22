dbaf7c1a8f507be5e00800e6bd3b42095dcf09d4
serii <seriiburduja@gmail.com>
2016-12-22 20:28:26 +0200

    site

---
+++ build/js/custom.js
@@ -0,0 +1,2 @@
+!function(t){t(".js-dropdown-arrow").on("click",function(r){r.preventDefault();var e=t(this).attr("href");t(e).slideToggle();var a=t(this).attr("data-target");t(a).slideToggle(500)})}(jQuery);
+//# sourceMappingURL=custom.js.map
