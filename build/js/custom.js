bfa5ada087bace6e293cc0b2b8faf04ad7672d08
serii <seriiburduja@gmail.com>
2016-12-24 16:09:26 +0200

    finish header

--- build/js/custom.js
+++ build/js/custom.js
@@ -1,2 +1,2 @@
-!function(t){t(".js-dropdown-arrow").on("click",function(r){r.preventDefault();var e=t(this).attr("href");t(e).slideToggle();var a=t(this).attr("data-target");t(a).slideToggle(500)})}(jQuery);
+!function(e){e(".js-dropdown-arrow").on("click",function(t){t.preventDefault();var a=e(this).attr("href");e(a).slideToggle();var i=e(this).attr("data-target");e(i).slideToggle(500)}),e("#js-main-slider").slick({infinite:!0,speed:3e3,autoplay:!1,autoplaySpeed:1e4,dots:!0,mobileFirst:!0,pauseOnHover:!1,fade:!0})}(jQuery);
 //# sourceMappingURL=custom.js.map
