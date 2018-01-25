5f5f21f15d0b82568cf75e2782e5d74204f5675a
serii <seriiburduja@gmail.com>
2018-01-25 10:04:39 +0200

    change portfolio

--- Portfolio/build/assets/js/main.js
+++ Portfolio/build/assets/js/main.js
@@ -2,20 +2,20 @@ $(function () {
   /*
       https://www.jqueryscript.net/animation/Smooth-Mouse-Wheel-Scrolling-Plugin-With-jQuery-easeScroll.html
       ===========================*/
-  $("html").easeScroll({
-    frameRate: 60,
-    animationTime: 1000,
-    stepSize: 90,
-    pulseAlgorithm: 1,
-    pulseScale: 8,
-    pulseNormalize: 1,
-    accelerationDelta: 20,
-    accelerationMax: 1,
-    keyboardSupport: true,
-    arrowScroll: 50,
-    touchpadSupport: true,
-    fixedBackground: true
-  });
+      $("html").easeScroll({
+        frameRate: 60,
+        animationTime: 1000,
+        stepSize: 90,
+        pulseAlgorithm: 1,
+        pulseScale: 8,
+        pulseNormalize: 1,
+        accelerationDelta: 20,
+        accelerationMax: 1,
+        keyboardSupport: true,
+        arrowScroll: 50,
+        touchpadSupport: true,
+        fixedBackground: true
+      });
 
   /* slider
   ====================================================*/
@@ -27,7 +27,7 @@ $(function () {
   });
 
   $('#slider-small').slick({
-    slidesToShow: 7,
+    slidesToShow: 6,
     slidesToScroll: 1,
     asNavFor: '#slider',
     centerMode: true,
